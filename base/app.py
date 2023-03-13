9d2d8482c9b6ae63ee66aea532c406d67cdc77eb
Kirill Litoshko <kovzik08@gmail.com>
2023-03-13 17:03:47 +0300

    updated main app builder and replace env var by token

--- base/app.py
+++ base/app.py
@@ -1,5 +1,8 @@
 from aiohttp import web
 from utils.ngrok import get_base_url
+from utils.applications import init_bot_application
+from config.tokens import BotTokens
+from config.settings import WEBHOOK_PATH
 
 
 async def on_startup(app: web.Application):
@@ -15,4 +18,6 @@ async def main_app() -> web.Application:
     app = web.Application()
     app.on_startup.append(on_startup)
     app.on_shutdown.append(on_shutdown)
+    for bot in BotTokens:
+        app.add_subapp(f'{WEBHOOK_PATH}/{bot.value}', init_bot_application(bot))
     return app
--- utils/ngrok.py
+++ utils/ngrok.py
@@ -1,11 +1,12 @@
 import os
 from config import settings
+from config import tokens
 
 
 def get_base_url():
     if not settings.BASE_URL:
         from pyngrok import conf, ngrok
-        ngrok.set_auth_token(os.environ.get("NGROK_TOKEN"))  # replace with token from configs
+        ngrok.set_auth_token(tokens.Tokens.ngrok.value)  # replace with token from configs
         conf.get_default().region = "eu"
         tunnel = ngrok.connect(settings.APP_PORT)
         settings.BASE_URL = tunnel.public_url.replace("http", "https")

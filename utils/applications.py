debf936914bd5959b1874bbe25f751964fde41c5
Kirill Litoshko <kovzik08@gmail.com>
2023-03-13 14:41:08 +0300

    added web application initialization for bot

---
+++ utils/applications.py
@@ -0,0 +1,29 @@
+from aiohttp import web
+from handlers.web import proceed_update
+from config import settings, dispatchers
+
+
+async def on_sub_startup(app):
+    bot_name = app["bot_name"]
+    bot_token = app["bot_token"]
+    app['dp'] = await dispatchers.DISPATCHERS[bot_name](bot_token)
+    url = f"{settings.BASE_URL}/tg/webhook/{app['bot_token']}/bot"
+    await app['dp'].bot.set_webhook(url)
+
+
+async def on_sub_shutdown(app):
+    dp = app['dp']
+    await dp.bot.delete_webhook()
+    current_session = await dp.bot.get_session()
+    if not current_session.closed:
+        await current_session.close()
+
+
+def init_bot_application(bot):
+    app = web.Application()
+    app["bot_token"] = bot.value
+    app["bot_name"] = bot.name
+    app.on_startup.append(on_sub_startup)
+    app.on_shutdown.append(on_sub_shutdown)
+    app.router.add_post("/bot", proceed_update)
+    return app
